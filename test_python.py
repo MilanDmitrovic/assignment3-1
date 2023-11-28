2f2537ceb07c10aaa606f1bab9c18bdfd922ba22
MilanDmitrovic <152094511+MilanDmitrovic@users.noreply.github.com>
2023-11-28 17:30:03 +0100

    Update test_python.py
    Ispravak koda

--- test_python.py
+++ test_python.py
@@ -1,8 +1,12 @@
+
 someNumber = 10
 
-if(someNumber <= 0):
-    print("Over zerro")
-elif(someNumber < 0):
-    print("Bellow zerro")
+# Proveravamo da li someNumber ima vrednost veću od nule
+if someNumber > 0:
+    print("Above zero")
+# Ako prvi uslov nije ispunjen, proveravamo da li someNumber ima vrednost manju od nule
+elif someNumber < 0:
+    print("Below zero")
+# Ako ni jedan od prethodnih uslova nije ispunjen, ispisujemo "Zero"
 else:
-    print("zerro")
+    print("Zero")
